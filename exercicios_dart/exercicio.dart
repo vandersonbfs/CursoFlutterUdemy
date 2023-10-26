8c64a272873f0b92efb95e5f3e3cd586ba680662
Vanderson Batista <106192524+vandersonbfs@users.noreply.github.com>
2023-10-26 10:10:39 -0300

    teste

--- exercicios_dart/exercicio.dart
+++ exercicios_dart/exercicio.dart
@@ -1,5 +1,15 @@
+/** int soma(int a, int b) {
+  return a + b;
+}
+
+class Produto {
+  String nome;
+  double preco;
+}
+*/
 main() {
   print('Primeiro Programa!');
+  /** 
   int a = 3;
   double b = 3.1;
   bool estaChovendo = true;
@@ -29,4 +39,19 @@ main() {
   print(conjunto.length);
   print('Conjunto é um Set?');
   print(conjunto is Set);
+  */
+
+  /**  Funções
+
+  final r = soma(2, 3);
+  print('O valor da soma é: $r');
+  
+
+  Classes
+
+  var p1 = new Produto();
+  p1.nome = 'Lapis';
+  p1.preco = 4.59;
+  print('O produto ${p1.nome} tem preço ${p1.preco}');
+  */
 }
