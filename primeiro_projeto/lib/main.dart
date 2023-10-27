95440a1545b596af2823491fc4d366802b895cac
Vanderson Batista <106192524+vandersonbfs@users.noreply.github.com>
2023-10-27 14:20:20 -0300

    Aula 47. Desafio Novo Widget

--- primeiro_projeto/lib/main.dart
+++ primeiro_projeto/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './questao.dart';
+import './resposta.dart';
 
 void main() => runApp(const PerguntaApp());
 
@@ -28,18 +29,9 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: [
             Questao(perguntas[_perguntaSelecionada]),
-            ElevatedButton(
-              onPressed: _responder,
-              child: const Text('Resposta 1'),
-            ),
-            ElevatedButton(
-              onPressed: _responder,
-              child: const Text('Resposta 2'),
-            ),
-            ElevatedButton(
-              onPressed: _responder,
-              child: const Text('Resposta 3'),
-            ),
+            const Resposta('Resposta 1'),
+            const Resposta('Resposta 2'),
+            const Resposta('Resposta 3'),
           ],
         ),
       ),
--- primeiro_projeto/lib/questao.dart
+++ primeiro_projeto/lib/questao.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
+//Criando outro componente
 class Questao extends StatelessWidget {
+  //Criando um construtor
   final String texto;
 
   const Questao(this.texto, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text(texto);
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.all(10),
+        child: Text(
+          texto,
+          style: TextStyle(fontSize: 28),
+          textAlign: TextAlign.center,
+        ));
   }
 }
