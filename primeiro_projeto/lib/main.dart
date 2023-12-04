b1772ef01028b190d6f8b187ea9255a7d589b024
Vanderson Batista <106192524+vandersonbfs@users.noreply.github.com>
2023-12-04 10:40:10 -0300

    Aula 51 Lista para Widgets #01

--- primeiro_projeto/lib/main.dart
+++ primeiro_projeto/lib/main.dart
@@ -30,6 +30,13 @@ class _PerguntaAppState extends State<PerguntaApp> {
       },
     ];
 
+    List<Widget> respostas = [];
+
+    for (String textoResp
+        in perguntas[_perguntaSelecionada]['respostas'] as List) {
+      respostas.add(Resposta(textoResp, _responder));
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -38,9 +45,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: [
             Questao(perguntas[_perguntaSelecionada]['texto'] as String),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
+            ...respostas,
           ],
         ),
       ),
