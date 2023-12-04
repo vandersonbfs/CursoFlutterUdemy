0acc74bc8266358f06d9d4fd26f1bc801b8a337a
Vanderson Batista <106192524+vandersonbfs@users.noreply.github.com>
2023-12-04 11:26:16 -0300

    Aula 55 Exibindo resultado

--- primeiro_projeto/lib/main.dart
+++ primeiro_projeto/lib/main.dart
@@ -59,7 +59,12 @@ class _PerguntaAppState extends State<PerguntaApp> {
                   ...respostas.map((t) => Resposta(t, _responder)).toList(),
                 ],
               )
-            : null,
+            : Center(
+                child: Text(
+                  'Parabéns!',
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
       ),
     );
   }
