b2340a8cee048d0c3686ed2240eb5c88640e2ec0
Vanderson Batista <106192524+vandersonbfs@users.noreply.github.com>
2023-12-06 18:17:39 -0300

    Aula 84 Criando o Projeto Despesas Pessoais

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
+// Aula 84 Criando o Projeto Despesas Pessoais
+// Inicialmente vamos criar uma StatelessWidget e importar o Material
+
 main() => runApp(const ExpensesApp());
+
+// Vamos criar uma classe chamada ExpensesApp, que extende uma StatelessWidget
+// que retorna o MaterialApp, com uma home, que chama uma classe chamada
+//MyHomeApp.
 
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({Key? key}) : super(key: key);
@@ -10,6 +17,9 @@ class ExpensesApp extends StatelessWidget {
     return const MaterialApp(home: MyHomePage());
   }
 }
+
+// Vamos criar uma classe chamada MyHomePage que contem uma build que retorna
+//um child com uma Text, com o texte 'Versão Inicial'.
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
