d1a7ced7c6475f526cae9a4285f9aec6fa8bbf7f
Vanderson Batista <106192524+vandersonbfs@users.noreply.github.com>
2023-12-06 16:16:16 -0300

    Aula 68 Conclusão do Módulo

--- primeiro_projeto/lib/main.dart
+++ primeiro_projeto/lib/main.dart
@@ -44,8 +44,13 @@ class _PerguntaAppState extends State<PerguntaApp> {
         _pontuacaoTotal += pontuacao;
       });
     }
+  }
 
-    print(_pontuacaoTotal);
+  void _reiniciarQuestionario() {
+    setState(() {
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
+    });
   }
 
   bool get temPerguntaSelecionada {
@@ -65,7 +70,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                 perguntaSelecionada: _perguntaSelecionada,
                 quandoResponder: _responder,
               )
-            : const Resultado(),
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
       ),
     );
   }
