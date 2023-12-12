bae996d9458d183432459b04166024a8b8f41a83
Vanderson Batista <106192524+vandersonbfs@users.noreply.github.com>
2023-12-12 15:33:43 -0300

    arquivo igual o da aula

--- expenses/lib/homePage.dart
+++ expenses/lib/homePage.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:expenses/models/transaction.dart';
 
 class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+  Homepage({super.key});
+
+  final transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Conectiva - Despesas Pessoais'),
+        title: const Text('Despesas Pessoais'),
       ),
       body: const Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Card(
-            child: Text('Grafico'),
+            child: Text('Gráfico'),
           ),
           Card(
-            child: Text('Lista de Transações'),
+            child: Text('A Lista de Transações'),
           )
         ],
       ),
--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,23 +1,59 @@
 import 'package:flutter/material.dart';
-
-import 'homePage.dart';
+import 'models/transaction.dart';
 
 main() => runApp(const ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MyHomePage());
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Homepage();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
   }
 }
